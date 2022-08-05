f9f2aaad6b4060e7b13569f5989413ae1bb208db
Solomon13693 <adeoyesolomon2693@gmail.com>
2022-08-05 09:44:10 +0100

    header-fixed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
 
             </div>
 
-            <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12"></div>
+            <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/6"></div>
 
         </nav>
 
